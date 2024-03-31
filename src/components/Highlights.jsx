476d58d4a2de37e669ca2eeb9b7d3b44ab80de0e
tachyonok <tachyonparticle123@gmail.com>
2024-04-01 00:52:44 +0530

    half video animations done

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,40 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+import VideoCarousal from "./VideoCarousal";
+
 const Highlights = () => {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      duration: 1,
+      stagger: 0.25,
+      y: 0,
+    });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="mb-12 w-full md:flex items-end justify-between">
+        <h1 id="title" className="section-heading">
+          Get the HighLights
+        </h1>
+        <div className="flex flex-wrap items-end gap-5">
+          <p className="link">Watch the film</p>
+          <img src={watchImg} alt="" className="ml-2" />
+          <p className="link">Watch the event</p>
+          <img src={rightImg} alt="" className="ml-2" />
+        </div>
+      </div>
+      <VideoCarousal />
+    </section>
+  );
 };
 
 export default Highlights;
