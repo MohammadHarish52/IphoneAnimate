c27263b6c49f000d5d9f3d70fe152763fa2687b0
tachyonok <tachyonparticle123@gmail.com>
2024-03-31 17:09:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+}
+
+export default App;
