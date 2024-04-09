840fa24715177062dbdf0f9a5725a795b4f4f82a
tachyonok <tachyonparticle123@gmail.com>
2024-04-09 22:13:09 +0530

    iphone added in the scene

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./index.css";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
+import Model from "./components/Model";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
