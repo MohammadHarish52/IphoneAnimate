382f8d3b9ad358a827ad60b41a92f50b261fe294
tachyonok <tachyonparticle123@gmail.com>
2024-04-13 14:53:12 +0530

    gaming section added

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -1,8 +1,11 @@
-import { chipImg } from "../utils";
+import { chipImg, frameImg, frameVideo } from "../utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useRef } from "react";
+import { animatewithGsap } from "../utils/animation";
 
 const Chip = () => {
+  const videoRef = useRef();
   useGSAP(() => {
     gsap.from("#chip", {
       scrollTrigger: {
@@ -13,6 +16,12 @@ const Chip = () => {
       scale: 2,
       duration: 2,
       ease: "power2.inOut",
+    });
+    animatewithGsap(".g_fadeIn", {
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 1,
     });
   });
   return (
@@ -25,8 +34,61 @@ const Chip = () => {
           <h2 className="hiw-title">
             A17 Pro chip.
             <br />A monster win for gaming.
-            <p className="hiw-subtitle"></p>
           </h2>
+          <p className="hiw-subtitle">
+            It's here . The biggest redesign in the history of Apple GPU's.
+          </p>
+        </div>
+        <div className="mt-10 md:mt-20 mb-14">
+          <div className="relative h-full flex-center">
+            <div className="overflow-hidden">
+              <img
+                src={frameImg}
+                alt=""
+                className="bg-transparent relative z-10 "
+              />
+            </div>
+            <div className="hiw-video">
+              <video
+                className="pointer-events-none"
+                playsInline
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={frameVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+          <p className="text-gray font-semibold text-center mt-3">
+            Honkai:Star Rail
+          </p>
+        </div>
+
+        <div className="hiw-text-container">
+          <div className="flex-1 flex justify-center flex-col">
+            <p className="hiw-text g_fadeIn">
+              A17 pro is an entirely new class of iPhone chip that delivers our{" "}
+              <span className="text-white">
+                Best Graphics performance by far
+              </span>
+            </p>
+
+            <p className="hiw-text g_fadeIn">
+              Mobile{" "}
+              <span className="text-white">
+                games will look and feel so immersive
+              </span>
+              , with incredibly detailed environments and characters
+            </p>
+          </div>
+
+          <div className="flex-1 justify-center flex-col g_fadeIn">
+            <p className="hiw-text">New</p>
+            <p className="hiw-bigtext">Pro-class GPU</p>
+            <p className="hiw-text">With 6 cores</p>
+          </div>
         </div>
       </div>
     </section>
